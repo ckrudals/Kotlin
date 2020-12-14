0e625a75b9ad8248d98858559edd71f76a2cf70d
ckrudals <2004ckck@naver.com>
2020-12-14 17:34:15 +0900

    var, val 의 차이를 알수있다

---
+++ src/Var/Var/val.kt
@@ -0,0 +1,17 @@
+package Var.Var
+
+//Var 변수
+//Val 상수
+//fun main()함수
+
+var num = 10;
+val arr = 10;
+fun main(String: Array<String>) {
+
+    println(num)
+    println(arr)
+
+    num=100
+    println(num)
+
+}
