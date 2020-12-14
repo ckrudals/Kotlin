562c98290c850604346cd76492807e3fb9cf6d4c
ckrudals <2004ckck@naver.com>
2020-12-14 18:08:40 +0900

    변수 선언 방법 var,val  :자료형 : 값

---
+++ src/Var/Var/DataType.kt
@@ -0,0 +1,15 @@
+package Var.Var
+
+//변수 선언 방법 var,val  :자료형 : 값
+var number = 10
+var number1: Int = 20
+var array: String = "안녕"
+val float: Float = 20.5f
+
+fun main(araay: Array<String>){
+
+    println(number)
+    println(number1)
+    println(array)
+    println(float)
+}
