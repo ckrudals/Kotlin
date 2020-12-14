37ac84d7e1d80ec43085363d36254583f9c7dbe5
ckrudals <2004ckck@naver.com>
2020-12-14 18:08:20 +0900

    Null 쓰는법을 알수있다

---
+++ src/Var/Var/VarvalTest.kt
@@ -0,0 +1,32 @@
+package Var.Var
+
+
+var sum = 1 + 2 + 3 + 4 + 5
+var b = "1"
+var c = b.toInt()
+var d = b.toFloat()
+
+var e = "kyungmin"
+var f = "my name is kyung min"
+
+//Null
+//존재하지 않음
+
+//var abc: Int=null ?을 붙이면 null을 가질수 있음
+var abc: Int? = null //문자열이 아님
+var abc2: Double? = null
+var g=sum+3
+
+fun main(array: Array<String>) {
+
+    println(sum)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(abc)
+    println(abc2)
+    println(g)
+
+}
